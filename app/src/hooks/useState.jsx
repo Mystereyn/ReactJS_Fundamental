843f2306bc32ee33247243a5a63d51c82641eeeb
Mystereyn <teukureynaldi106@gmail.com>
2023-03-04 21:13:41 +0700

    learn a useState from yt : ErdeCode

---
+++ app/src/hooks/useState.jsx
@@ -0,0 +1,10 @@
+import React , {useState} from "react";
+
+
+function useState() {
+    // usesState can sign a value with string, null, define or number.
+    // the value, must sign with our needs
+    const [counter, setCounter] = useState(0);
+}
+
+export default useState;
