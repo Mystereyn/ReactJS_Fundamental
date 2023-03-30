4a3385a3f9f754387433d6b1fedff99edb542a13
Mystereyn <teukureynaldi106@gmail.com>
2023-03-30 09:07:05 +0700

    add roboto font and later doing to the tasks

--- todo-app/src/components/ToDoing.jsx
+++ todo-app/src/components/ToDoing.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Button  from "react-bootstrap/Button";
+import {Button,Alert}  from "react-bootstrap";
 import Stack from '@mui/material/Stack';
 import {useState} from "react";
 
@@ -9,22 +9,31 @@ import "../ToDoing.css";
 
 function ToDoing() {
 
-    const [showAlert, setShowAlert] = useState(false);
-
-    const toggle = () => {
-        alert("You have clicked this you can close now" + setShowAlert(showAlert));
+    // const [showAlert, setShowAlert] = useState(false);
+    // const toggle = () => {
+    //     alert("You have clicked this you can close now");
+    // }
+    
+    // make a variable that for saving a value which is that for inline styling
+    // header
+    const headerStyling = {
+        color: "#5B33FF",
+        textAlign: "center",
+        fontFamily : 'roboto'
     }
-        
     return (
         <div className='To-Doing'>
             <div className="form">
-                <div className="buttons-group-row">
-                    {/* Stack is a  */}
+                <div id="header-title-ToDoing">
+                    <header style={headerStyling}> ToDoing App By Mystereyn!</header>
+                </div>
+                <span className="buttons-row">
                     <Stack direction="row" spacing={0.5}>
-                        <Button variant='primary' onClick={toggle}>Add/Tambahkan</Button>
+                        {/* <Button variant='primary' onClick={toggle}>Add/Tambahkan</Button> */}
+                        <Button variant='primary'>Add/Tambahkan</Button>
                         <Button variant='danger'>Delete/Hapus</Button>
-                    </Stack>    
-                </div
+                    </Stack>
+                </span>
             </div>
         </div>
     )
