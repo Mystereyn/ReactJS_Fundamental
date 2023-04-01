9f4680d9f6ce5c7142916f0c22d12c799604c6b6
Mystereyn <teukureynaldi106@gmail.com>
2023-04-01 12:04:15 +0700

    it's done, now giving a logic or pull data from database

--- todo-app/src/components/ToDoing.jsx
+++ todo-app/src/components/ToDoing.jsx
@@ -1,39 +1,49 @@
 import * as React from 'react';
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import {Button,Alert}  from "react-bootstrap";
+import {Button}  from "react-bootstrap";
 import Stack from '@mui/material/Stack';
-import {useState} from "react";
 
+import {useState} from "react";
+// An input from mui that require for user type their activies list.
+import Input  from '@mui/joy/Input';
+
+// for manual styling background, that doesnt need another 3rd party libraries.
 import "../ToDoing.css";
 
-function ToDoing() {
 
-    // const [showAlert, setShowAlert] = useState(false);
-    // const toggle = () => {
-    //     alert("You have clicked this you can close now");
-    // }
-    
-    // make a variable that for saving a value which is that for inline styling
-    // header
-    const headerStyling = {
-        color: "#5B33FF",
-        textAlign: "center",
-        fontFamily : 'roboto'
-    }
+function ToDoing() {
     return (
-        <div className='To-Doing'>
-            <div className="form">
-                <div id="header-title-ToDoing">
-                    <header style={headerStyling}> ToDoing App By Mystereyn!</header>
-                </div>
-                <span className="buttons-row">
-                    <Stack direction="row" spacing={0.5}>
-                        {/* <Button variant='primary' onClick={toggle}>Add/Tambahkan</Button> */}
-                        <Button variant='primary'>Add/Tambahkan</Button>
-                        <Button variant='danger'>Delete/Hapus</Button>
-                    </Stack>
-                </span>
+        <div className='To-Doing-App'>
+            <div className='Container'>
+                    <Container maxWidth="sm">
+                        <div id="header-title-ToDoing">
+                            <header className="text-md-center" style = {{ fontStyle: "italic", color: "#800080" }}>
+                                <h3> ToDoing App By Mystereyn!</h3>
+                            </header>
+                        </div>
+                        <div id="input-activity/ies">
+                            <Input placeholder='Type your activy/ies here'
+                                   variant="outlined"
+                                   color="neutral">
+                            </Input>
+                        </div>
+                        <div className='btn'>
+                            <span className="buttons-row">
+                                <Box sx={{
+                                    py:2,
+                                    display: 'grid',
+                                    gap:2,
+                                    alignItems: 'center',
+                                    flexWrap: 'wrap'
+                                }}/>
+                                <Stack direction="row" spacing={0.5}>
+                                    <Button variant='primary'>Add/Tambahkan</Button>
+                                    <Button variant='danger'>Delete/Hapus</Button>
+                                </Stack>
+                            </span>
+                        </div>
+                    </Container>           
             </div>
         </div>
     )
